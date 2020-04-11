9e4ad87d2b719db17d9c20d82fabe6875853eec0
Hamza Anonime <61266493+Anonime7x@users.noreply.github.com>
2020-04-11 02:53:09 +0100

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,54 @@
+#
+#   _____   _   _   _____   _   _        
+#|  ___| | | | | /  ___| | | / /       
+#| |__   | | | | | |     | |/ /        
+#|  __|  | | | | | |     | |\ \        
+#| |     | |_| | | |___  | | \ \       
+#|_|     \_____/ \_____| |_|  \_\
+#          Hacking Tools by Hamzaanonime 
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/fuckadmin/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/Anonime7x
+
+clear
+
+mkdir Anonime7x
+
+clear
+
+cd technicaldada
+
+clear
+
+git clone https://github.com/Anonime7x/fuckadmin.git
+
+clear
+
+cd fuckadmin
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
